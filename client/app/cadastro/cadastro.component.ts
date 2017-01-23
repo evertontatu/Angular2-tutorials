807b6fafdb7bc48256f6137a8617c4c2e4795322
Everton Boni <evertonferreira.de@gmail.com>
2017-01-23 17:16:52 -0200

    add event binding

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
     moduleId: module.id,
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CadastroComponent {
 
+    foto: FotoComponent = new FotoComponent();
+
+    cadastrar(event) {
+        event.preventDefault();
+        
+        console.log(this.foto);
+    }
 }
