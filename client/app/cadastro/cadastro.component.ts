36186c4927a9ee07abaf27776f426f90dbdb1dbe
Everton Boni <evertonferreira.de@gmail.com>
2017-01-25 15:36:14 -0200

    get url params for edit FotoComponent

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FotoService } from '../foto/foto.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -13,10 +14,24 @@ export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
     meuForm: FormGroup;
     service: FotoService;
+    route: ActivatedRoute;
 
-    constructor(service: FotoService, fb: FormBuilder) {
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute) {
 
         this.service = service;
+        this.route = route;
+
+        this.route.params.subscribe(params => {
+            let id = params['id'];
+
+            this.service
+                .buscaPorId(id)
+                .subscribe(
+                    foto => this.foto = foto,
+                    err => console.log(err)
+                );
+        });
+
         this.meuForm = fb.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
             url: ['', Validators.required],
