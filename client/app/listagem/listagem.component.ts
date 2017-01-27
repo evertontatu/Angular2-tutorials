59637d6fc81f428398ff373fbd9cd7f471df00f3
Everton Boni <evertonferreira.de@gmail.com>
2017-01-27 11:11:48 -0200

    add fadeOut when remove FotoComponent

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoService } from '../foto/foto.service';
 import { FotoComponent } from '../foto/foto.component';
+import { PainelComponent } from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
@@ -23,17 +24,21 @@ export class ListagemComponent {
             }, err => console.log(err));
     }
 
-    remove(foto) {
+    remove(foto: FotoComponent, painel: PainelComponent) {
 
         this.service
             .remove(foto)
             .subscribe(
                 () => {
-                    let novasFotos = this.fotos.slice(0);
-                    let indice = novasFotos.indexOf(foto);
-                    novasFotos.splice(indice, 1);
-                    this.fotos = novasFotos;
-                    this.mensagem = 'Foto removida com sucesso';
+
+                    painel.fadeOut(() => {
+                        let novasFotos = this.fotos.slice(0);
+                        let indice = novasFotos.indexOf(foto);
+                        novasFotos.splice(indice, 1);
+                        this.fotos = novasFotos;
+                        this.mensagem = 'Foto removida com sucesso';
+                    });
+
                 },
                 err => console.log(err);
                 this.mensagem = 'Não foi possível remover a foto';
--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input , OnInit} from '@angular/core';
+import { Component, Input , OnInit, ElementRef} from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -9,6 +9,11 @@ import { Component, Input , OnInit} from '@angular/core';
 export class PainelComponent implements OnInit {
 
     @Input() titulo: string;
+    elemento: ElementRef;
+
+    constructor(elemento: ElementRef) {
+        this.elemento = elemento;
+    }
 
     ngOnInit() {
 
@@ -16,5 +21,10 @@ export class PainelComponent implements OnInit {
             this.titulo.length > 7
             ? this.titulo.substr(0,7) + '...'
             : this.titulo;
+    }
+
+    fadeOut(cb) {
+
+        $(this.elemento.nativeElement).fadeOut(cb);
     }
 }
